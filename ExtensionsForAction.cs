a22f5c1d53ebe5bb31b18ff08627e105ccb8f2ec
Andy-flower-github <86655544+Andy-flower-github@users.noreply.github.com>
2021-06-29 20:01:27 +0800

    Add files via upload
    Box.cs: 定義 struct Box. 及建立 F.Boxing(value, Box(value)) 來實現 FP Return monad
    ExtensionsForBox.cs: 將操作Box的Function. 用 Extension 的定義.
    ExtensionsForAction.cs: f(action, func)
    BoxBingoTest.cs: 寫一個UnitTest , 來測試 Box 的功能

---
+++ ExtensionsForAction.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Unit = System.ValueTuple;
+
+namespace Andy.Functional
+{
+    /// <summary>
+    /// Andy -> Action -> Function
+    /// </summary>
+    public static class ExtensionsForAction
+    {
+        /// <summary>
+        /// Andy -> 提供 Action To Fnnc 的回傳值. Unit = System.ValueTuple
+        /// </summary>
+        public static Unit Unit() => default(Unit);
+
+        /// <summary>
+        /// Andy -> Action -> Func(Action, Unit)  
+        /// </summary>
+        public static Func<Unit> ToFunc(this Action action)
+            => () => { action(); return Unit(); };
+
+        /// <summary>
+        /// Andy -> Action[t] -> Func(Action[t], Unit)  
+        /// </summary>
+        public static Func<T, Unit> ToFunc<T>(this Action<T> action)
+            => (t) => { action(t); return Unit(); };
+
+        /// <summary>
+        /// Andy -> Action[t1, t2] -> Func(Action[t1, t2], Unit)  
+        /// </summary>
+        public static Func<T1, T2, Unit> ToFunc<T1, T2>(this Action<T1, T2> action)
+                => (t1, t2) => { action(t1, t2); return Unit(); };
+    }
+}
